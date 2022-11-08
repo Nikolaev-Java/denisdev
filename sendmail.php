9751ef8bcd95528066e07d52de838d24d66e5977
Denis <epselent33@gmail.com>
2022-11-08 12:55:12 +0500

    тест отправки формы php

---
+++ sendmail.php
@@ -0,0 +1,11 @@
+$name = $_POST['name'];
+$email = $_POST['email'];
+$title = $_POST['title'];
+$message = $_POST['message'];
+echo $name;
+echo "<br>"
+echo $email;
+echo "<br>"
+echo $title;
+echo "<br>"
+echo $message;
