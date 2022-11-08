61ed4473e29d94286e787baf7677487ba55d2362
Denis <epselent33@gmail.com>
2022-11-08 17:57:33 +0500

    отправка формы проверка

--- sendmail.php
+++ sendmail.php
@@ -3,11 +3,29 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $title = $_POST['title'];
 $message = $_POST['message'];
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $title;
-echo "<br>";
-echo $message;
+$name = htmlspecialchars($name);
+$email = htmlspecialchars($email);
+$title = htmlspecialchars($title);
+$message = htmlspecialchars($message);
+$name = strip_tags($name);
+$email = strip_tags($email);
+$title = strip_tags($title);
+$message = strip_tags($message);
+$name = stripslashes($name);
+$email = stripslashes($email);
+$title = stripslashes($title);
+$message = stripslashes($message);
+$name = urldecode($name);
+$email = urldecode($email);
+$title = urldecode($title);
+$message = urldecode($message);
+$name = trim($name);
+$email = trim($email);
+$title = trim($title);
+$message = trim($message);
+if (mail("epselent33@gmail.com", "заявка с сайта" . $title, "Имя: " . $name . "Email: " . $email . "Сообщение: " . $message . "From: info@denisdev.com \r\n")) {
+	echo "ok";
+} else {
+	echo "not";
+}
 ?>
